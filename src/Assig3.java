8411fc0f2e5e28976018ad79b9d92a7193f2d8bd
Huy <huy.nguyen@vivalnk.com>
2017-01-19 14:45:24 -0800

    Draft of Card Class

--- src/Assig3.java
+++ src/Assig3.java
@@ -6,12 +6,18 @@ public class Assig3
     public static void main(String[] args)
     {
 	// TODO Auto-generated method stub
+    	
+    	
+    	Card myCard = new Card('A', Card.Suit.CLUBS);
+    	
+    	System.out.println(myCard.toString());
 
     }
 }
 
 class Card
 {
+	
 	private char value;
     private Suit suit;
     private boolean errorFlag;
@@ -21,17 +27,73 @@ class Card
     	CLUBS, DIAMONDS, HEARTS, SPADES
     }
     
-    public static Card getCard(char value, Suit suit)
+    public Card(char value, Suit suit)
     {
-    	Card myCard = new Card();
-    	return myCard;
+    	
+    	this.value = value;
+    	this.suit = suit;
     }
     
-    public static Card getCard()	//method overload
+    public Card()	//method overload
     {
-    	Card myCard = new Card();
-       	myCard.value = 'A';
-       	String suit = "SPADES"; 
-    	return myCard;
+    	this.value = 'A';
+    	this.suit = suit.SPADES;
+    }
+    
+    public boolean set(char value, Suit suit)
+    {
+    	this.errorFlag = isValid(value, suit);
+    	return errorFlag;    	   
+    }
+    
+    private boolean isValid(char value, Suit suit)
+    {
+    	return true;
+    }
+    
+    public String toString()
+    {
+    	if (errorFlag == false)
+    	{
+    		return getValue() + " of " + getSuit();
+    	}else{
+    		return "ILLEGAL STATE";
+    	}
+    }
+    
+    public char getValue()
+    {
+    	return value;
+    }
+    
+    public String getSuit()
+    {
+    	switch (suit)
+    	{
+    	case CLUBS:
+    		return "Clubs";
+    	case DIAMONDS:
+    		return "Diamonds";
+    	case HEARTS:
+    		return "Hearts";    	
+    	}
+    	return "Spades";    	
+    }
+    
+    public boolean getErrorFlag()
+    {
+    	return errorFlag;
+    }
+    
+    
+//------------- Individual mutators not needed yet (?) --------------    
+    public void setValue(char value)
+    {
+    	this.value = value;
+    }
+    
+    public void setSuit(Suit suit)
+    {
+    	this.suit = suit;   	
     }
 }
